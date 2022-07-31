5fa7a21000743cade85205274f400607dc3df1cc
StefanoGalligani <stefanogalligani0987@gmail.com>
2022-07-31 12:10:39 +0200

    Updated punch

--- Assets/Scripts/Game/PunchScript.cs
+++ Assets/Scripts/Game/PunchScript.cs
@@ -7,11 +7,9 @@ public class PunchScript : MonoBehaviour
     public int damage = 1;
     public bool autoAlert;
     private bool isPunching;
-    private List<GameObject> obstacles;
 
     private void Start() {
         SetAlerted(autoAlert);
-        obstacles = new List<GameObject>();
         isPunching = false;
     }
 
@@ -23,31 +21,38 @@ public class PunchScript : MonoBehaviour
         if (transform.IsChildOf(other.transform)) return;
         if (isPunching) {
             if (other.gameObject.layer == LayerMask.NameToLayer("People")) {
-                other.transform.parent.GetComponent<PersonScript>().hit(damage, true, transform.parent.parent.gameObject);
+                other.transform.parent.GetComponent<PersonScript>().hit(damage, other.gameObject.name == "Head", transform.parent.parent.gameObject);
             }
             stopPunching();
-        } else if (!obstacles.Contains(other.gameObject)){
-            obstacles.Add(other.gameObject);
         }
     }
 
     private IEnumerator suddenStop() {
-        yield return new WaitForEndOfFrame();
+        yield return new WaitForSeconds(0.015f);
         stopPunching();
     }
 
-    private void OnCollisionExit(Collision other) {
-        if (transform.IsChildOf(other.transform)) return;
-        obstacles.Remove(other.gameObject);
+    private bool canPunch() {
+        Vector3 localCenter = GetComponent<SphereCollider>().center;
+        Vector3 center = transform.position + new Vector3(localCenter.x * transform.localScale.x, localCenter.y * transform.localScale.y, localCenter.z * transform.localScale.z);
+        float radius = GetComponent<SphereCollider>().radius * transform.localScale.z;
+        Collider[] overlaps = Physics.OverlapSphere(center, radius);
+
+        foreach (Collider overlap in overlaps)
+        {
+            if (!transform.IsChildOf(overlap.transform)) return false;
+        }
+        return true;
     }
 
     public void startPunching() {
         if (!isPunching) {
+            transform.parent.parent.GetComponent<Rigidbody>().AddForce(new Vector3(10, 10, 0), ForceMode.Impulse);
             gameObject.layer = LayerMask.NameToLayer("MovingPunch");
             isPunching = true;
             GetComponent<Animator>().SetBool("Punching", true);
             
-            if (obstacles.Count > 0)
+            if (!canPunch())
                 StartCoroutine(suddenStop());
         }
     }
